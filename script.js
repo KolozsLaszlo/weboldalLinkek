f4ae89035d16c493fcede3a36387b4552d379dab
KZS <KZS@a.a>
2024-06-04 09:43:16 +0200

    H1 title update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector("#container");
 const kattintasok = document.querySelector("#kattintasok");
 
-let katt_szam = 1;
+let katt_szam = 0;
 
 container.addEventListener("click", function () {
-  kattintasok.innerHTML = katt_szam;
   katt_szam++;
+  kattintasok.innerHTML = katt_szam;
 });
